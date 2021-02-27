582e9d8962b893d87173a1698e2cf96248b8994f
moabre <muhammadrehman21@gmail.com>
2021-02-27 13:11:41 -0600

    connected the mongodb database and created user schema

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  () => console.log('connected')
+);
+
+//Import Routes
+const authRoute = require('./routes/auth');
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log('server up and running'));
+app.listen(PORT, () => console.log(`server up and running on ${PORT} `));
